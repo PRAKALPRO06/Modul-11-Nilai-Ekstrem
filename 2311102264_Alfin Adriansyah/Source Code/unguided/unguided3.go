ff271fad7a87cd7d15705bf3de78fffe08a2f68b
Alfin <161679630+Alf1nn@users.noreply.github.com>
2024-11-24 20:11:22 +0700

    Add files via upload

---
+++ 2311102264_Alfin Adriansyah/Source Code/unguided/unguided3.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type arrBalita [100]float64
+
+func hitungMinMax(arrBerat arrBalita, bMin, bMax *float64) {
+	*bMin = arrBerat[0]
+	*bMax = arrBerat[0]
+
+	for i := 1; i < 4; i++ {
+		if arrBerat[i] < *bMin {
+			*bMin = arrBerat[i]
+		}
+		if arrBerat[i] > *bMax {
+			*bMax = arrBerat[i]
+		}
+	}
+}
+
+func rerata(arrBerat arrBalita) float64 {
+	var total float64
+	for i := 0; i < 4; i++ {
+		total += arrBerat[i]
+	}
+	return total / 4
+}
+
+func main() {
+	var beratBalita arrBalita
+	var bMin, bMax float64
+	var n int
+
+	fmt.Printf("Masukan banyak data berat balita : ")
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		fmt.Printf("Masukan berat balita ke-%d: ", i+1)
+		fmt.Scan(&beratBalita[i])
+	}
+	hitungMinMax(beratBalita, &bMin, &bMax)
+	avg := rerata(beratBalita)
+
+	fmt.Printf("Berat balita minimum: %.2f kg\n", bMin)
+	fmt.Printf("Berat balita maksimum: %.2f kg\n", bMax)
+	fmt.Printf("Rerata berat balita: %.2f kg\n", avg)
+}
